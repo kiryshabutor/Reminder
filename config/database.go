c77424d204fd896b5005dabe790f47aa3026ec5f
kiribu <kiryshabutor@gmail.com>
2026-01-19 19:54:35 +0300

    add db config file

---
+++ config/database.go
@@ -0,0 +1,63 @@
+package config
+
+import (
+	"fmt"
+	"os"
+	"time"
+
+	_ "github.com/jackc/pgx/v5/stdlib"
+	"github.com/jmoiron/sqlx"
+)
+
+type DatabaseConfig struct {
+	Host     string
+	Port     string
+	User     string
+	Password string
+	DBName   string
+	SSLMode  string
+}
+
+func LoadDatabaseConfig() *DatabaseConfig {
+	return &DatabaseConfig{
+		Host:     getEnv("DB_HOST", "localhost"),
+		Port:     getEnv("DB_PORT", "5432"),
+		User:     getEnv("DB_USER", "postgres"),
+		Password: getEnv("DB_PASSWORD", ""),
+		DBName:   getEnv("DB_NAME", "jwt_app"),
+		SSLMode:  getEnv("DB_SSLMODE", "disable"),
+	}
+}
+
+func (c *DatabaseConfig) ConnectionString() string {
+	return fmt.Sprintf(
+		"host=%s port=%s user=%s password=%s dbname=%s sslmode=%s",
+		c.Host, c.Port, c.User, c.Password, c.DBName, c.SSLMode,
+	)
+}
+
+func ConnectDatabase(config *DatabaseConfig) (*sqlx.DB, error) {
+	db, err := sqlx.Connect("pgx", config.ConnectionString())
+	if err != nil {
+		return nil, fmt.Errorf("не удалось подключиться к БД: %w", err)
+	}
+
+	// Проверка подключения
+	if err := db.Ping(); err != nil {
+		return nil, fmt.Errorf("не удалось проверить соединение с БД: %w", err)
+	}
+
+	// Настройка пула соединений
+	db.SetMaxOpenConns(25)
+	db.SetMaxIdleConns(5)
+	db.SetConnMaxLifetime(5 * time.Minute)
+
+	return db, nil
+}
+
+func getEnv(key, defaultValue string) string {
+	if value := os.Getenv(key); value != "" {
+		return value
+	}
+	return defaultValue
+}
